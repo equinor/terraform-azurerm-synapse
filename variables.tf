0b5dac09a7984a2d6619e8c774ca84dca31c32e9
ErlendT <erlth@equinor.com>
2023-09-27 08:04:24 +0200

    fix!: Removed log analytics destination type
    This is not used for the synapse resource https://learn.microsoft.com/en-us/azure/azure-monitor/reference/tables/azurediagnostics#resource-types

--- variables.tf
+++ variables.tf
@@ -186,14 +186,14 @@ variable "linked_services" {
   type = list(object({
     name : string
     type : string
-    type_properties_json : string
+    type_properties : map(any)
     additional_properties : optional(map(string), null)
-    annotations : optional(list(map(string)), null)
+    annotations : optional(list(string), [])
     description : optional(string, "")
     integration_runtime = optional(object({
       name : optional(string, null)
       parameters : optional(map(string), null)
-    }))
+    }), null)
     parameters : optional(map(string), null)
   }))
   default = []
@@ -219,17 +219,6 @@ variable "private_link_hub_tags" {
   description = "Tags for the private link hub."
   type        = map(string)
   default     = null
-}
-
-variable "firewall_rules" {
-  description = "List of firewall rules."
-  type = list(object({
-    name             = string
-    start_ip_address = string
-    end_ip_address   = string
-  }))
-
-  default = []
 }
 
 variable "tags" {
