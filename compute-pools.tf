4a277763e79dde45902ed12505c83469536cccea
ErlendT <erlth@equinor.com>
2024-01-23 12:02:42 +0100

    chore!: Removed SQL pool as a variable. This should instead be created directly using the submodule

--- compute-pools.tf
+++ compute-pools.tf
@@ -46,34 +46,3 @@ resource "azurerm_synapse_spark_pool" "this" {
 
   tags = each.value.tags
 }
-
-module "sql_pool" {
-  source   = "./modules/sql_pool"
-  for_each = var.sql_pools
-
-  name                      = each.value.name
-  workspace_id              = azurerm_synapse_workspace.this.id
-  sku_name                  = each.value.sku_name
-  create_mode               = each.value.create_mode
-  collation                 = each.value.collation
-  data_encrypted            = each.value.data_encrypted
-  recovery_database_id      = each.value.recovery_database_id
-  restore                   = each.value.restore
-  geo_backup_policy_enabled = each.value.geo_backup_policy_enabled
-  tags                      = each.value.tags
-
-  audit_storage_account_name           = data.azurerm_storage_account.audit_storage.name
-  audit_storage_account_resource_group = data.azurerm_storage_account.audit_storage.resource_group_name
-  audit_retention_in_days              = each.value.audit_retention_in_days
-  audit_log_monitoring_enabled         = each.value.audit_log_monitoring_enabled
-  alert_policy_state                   = each.value.alert_policy_state
-  disabled_alerts                      = each.value.disabled_alerts
-  alert_account_admins_email           = each.value.alert_account_admins_email
-  alert_email_addresses                = each.value.alert_email_addresses
-  alert_retention_days                 = each.value.alert_retention_days
-
-  recurring_scans                    = each.value.recurring_scans
-  vulnerability_assessment_baselines = each.value.vulnerability_assessment_baselines
-
-  workload_groups = each.value.workload_groups
-}
