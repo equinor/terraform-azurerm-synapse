b5ffe836df9c1b3747d24ab5ae14e8e109d08bf8
ErlendT <32453298+ErlendT@users.noreply.github.com>
2024-01-26 08:34:24 +0100

    fix: Add lifecycle ignore to node-count for sparkpools to avoid wrong updates (#12)

--- compute-pools.tf
+++ compute-pools.tf
@@ -45,4 +45,8 @@ resource "azurerm_synapse_spark_pool" "this" {
   spark_version       = each.value.spark_version
 
   tags = each.value.tags
+
+  lifecycle { # Ignore changes to node count as this triggers an update every time
+    ignore_changes = [node_count]
+  }
 }
