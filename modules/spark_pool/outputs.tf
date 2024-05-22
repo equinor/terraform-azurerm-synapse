4e6674e9b33ba041249c35cebabf867a919d6152
ErlendT <erlth@equinor.com>
2024-05-22 11:07:49 +0200

    fix: Add index to output

--- modules/spark_pool/outputs.tf
+++ modules/spark_pool/outputs.tf
@@ -1,7 +1,7 @@
 output "pool_id" {
   description = "The ID of this Spark Pool."
   # Conditionally output based on auto scale rules
-  value = var.auto_scale_max_node_count != null ? azurerm_synapse_spark_pool.auto_scale.id : azurerm_synapse_spark_pool.static_nodes.id
+  value = var.auto_scale_max_node_count != null ? azurerm_synapse_spark_pool.auto_scale[0].id : azurerm_synapse_spark_pool.static_nodes[0].id
 }
 
 output "diagnostic_setting_id" {
